bc48e8b33e2b8e8e37d47f0b5bd16d3bfedba976
camalzade_elvin <camalzade_elvin@outlook.com>
2018-12-20 21:14:24 +0400

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    interface IButton
+    {
+        string GetButtonDescription();
+    }
+
+    interface ITextBox
+    {
+        string GetTextBoxDescription();
+    }
+
+    class LinuxTextBox : ITextBox
+    {
+        public string GetTextBoxDescription()
+        {
+            return "Linux TextBox";
+        }
+    }
+    class WindowsTextBox : ITextBox
+    {
+        public string GetTextBoxDescription()
+        {
+            return "Windows TextBox";
+        }
+    }
+    class LinuxButton : IButton
+    {
+        public string GetButtonDescription()
+        {
+            return "Linux Button";
+        }
+    }
+    class WindowsButton : IButton
+    {
+        public string GetButtonDescription()
+        {
+            return "Windows Button";
+        }
+    }
+    interface IDialog
+    {
+        IButton CreateButton();
+        ITextBox CreateTextBox();
+    }
+    class WindowsDialog : IDialog
+    {
+        public IButton CreateButton()
+        {
+            return new WindowsButton();
+        }
+
+        public ITextBox CreateTextBox()
+        {
+            return new WindowsTextBox();
+        }
+    }
+    class LinuxDialog : IDialog
+    {
+        public IButton CreateButton()
+        {
+            return new LinuxButton();
+        }
+
+        public ITextBox CreateTextBox()
+        {
+            return new LinuxTextBox();
+        }
+    }
+
+    //Idialog,windowsDialog,linuxdialog
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
